191c271e3425d17fd44e3076c87667b2b80bd4d9
scorzy <scorzy88@gmail.com>
2019-10-20 12:06:25 +0200

    menu

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -9,8 +9,7 @@ import { MainService } from "../main.service";
 @Component({
   selector: "app-side-menu",
   templateUrl: "./side-menu.component.html",
-  styleUrls: ["./side-menu.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ["./side-menu.component.scss"]
 })
 export class SideMenuComponent implements OnInit {
   @Input() notCollapsed = false;
