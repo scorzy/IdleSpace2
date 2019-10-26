fb55f314964b3518375f126af98b6e57fc2da13f
scorzy <scorzy88@gmail.com>
2019-10-26 17:06:26 +0200

    ui

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -5,11 +5,13 @@ import {
   ChangeDetectionStrategy
 } from "@angular/core";
 import { MainService } from "../main.service";
+import { fadeIn } from "../animations";
 
 @Component({
   selector: "app-side-menu",
   templateUrl: "./side-menu.component.html",
-  styleUrls: ["./side-menu.component.scss"]
+  styleUrls: ["./side-menu.component.scss"],
+  animations: [fadeIn]
 })
 export class SideMenuComponent implements OnInit {
   @Input() notCollapsed = false;
