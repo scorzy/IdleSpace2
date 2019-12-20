3e90719a519d043bc0889b2988419d949ca52ac4
scorzy <scorzy88@gmail.com>
2019-12-20 19:28:32 +0100

    IdleSpace2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IdleAnt3
+# IdleSpace2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.3.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,14 +10,19 @@ describe("workspace-project App", () => {
 
   it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual("IdleAnt3 app is running!");
+    expect(page.getTitleText()).toEqual("IdleSpace2 app is running!");
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      } as logging.Entry)
+    );
   });
 });
