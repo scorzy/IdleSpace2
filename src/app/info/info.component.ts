e725127b6df64b736868942790250265c6527490
scorzy <scorzy88@gmail.com>
2020-07-26 23:12:38 +0200

    components info

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  OnDestroy,
+  AfterViewInit
+} from "@angular/core";
 import { INSPIRATION_PERCENT, FLEET_CAPACITY } from "../model/CONSTANTS";
+import { MainService } from "../main.service";
+import { ActivatedRoute } from "@angular/router";
+import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
+import { BaseComponentComponent } from "../base-component/base-component.component";
 
 @Component({
   selector: "app-info",
@@ -7,10 +18,39 @@ import { INSPIRATION_PERCENT, FLEET_CAPACITY } from "../model/CONSTANTS";
   styleUrls: ["./info.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent extends BaseComponentComponent
+  implements OnInit, OnDestroy, AfterViewInit {
   INSPIRATION_PERCENT = INSPIRATION_PERCENT;
   FLEET_CAPACITY = FLEET_CAPACITY;
-  constructor() {}
-
-  ngOnInit(): void {}
+  tabPosition = "left";
+  constructor(
+    ms: MainService,
+    cd: ChangeDetectorRef,
+    private route: ActivatedRoute,
+    public breakpointObserver: BreakpointObserver
+  ) {
+    super(ms, cd);
+  }
+  ngOnInit() {
+    this.subscriptions.push(
+      this.ms.updateEmitter.subscribe(() => {
+        this.cd.markForCheck();
+      }),
+      this.breakpointObserver
+        .observe(["(min-width: 599px)"])
+        .subscribe((state: BreakpointState) => {
+          this.tabPosition = state.matches ? "left" : "top";
+          this.cd.markForCheck();
+        })
+    );
+  }
+  ngAfterViewInit() {
+    super.ngAfterViewInit();
+    if (!this.breakpointObserver.isMatched("(min-width: 599px)")) {
+      setTimeout(() => {
+        this.tabPosition = "top";
+        this.cd.markForCheck();
+      });
+    }
+  }
 }
