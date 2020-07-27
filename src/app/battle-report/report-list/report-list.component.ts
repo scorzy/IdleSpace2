4800406b667146d6a042a57f83d46b9e069fe2ab
scorzy <scorzy88@gmail.com>
2020-07-27 22:07:14 +0200

    Added csv report download

--- src/app/battle-report/report-list/report-list.component.ts
+++ src/app/battle-report/report-list/report-list.component.ts
@@ -7,6 +7,7 @@ import {
 } from "@angular/core";
 import { BaseComponentComponent } from "src/app/base-component/base-component.component";
 import { FLEET_NUMBER } from "src/app/model/CONSTANTS";
+import { json2csv, csv2jsonAsync, json2csvAsync } from "json-2-csv";
 
 @Component({
   selector: "app-report-list",
@@ -30,5 +31,42 @@ export class ReportListComponent extends BaseComponentComponent
   ngOnDestroy() {
     this.ms.game.updateStats = true;
     super.ngOnDestroy();
+  }
+  async download(id: string) {
+    const report = this.ms.game.battleStats.find((rep) => rep.id === id);
+    if (!report) return;
+    // const json = JSON.stringify(report.stats);
+    let arr = [];
+    report.stats.forEach((stats) => {
+      let round = 1;
+      stats.rounds.concat(stats.total).forEach((st) => {
+        let rep: any = {};
+        rep.designId = stats.designId;
+        rep.player = stats.player;
+        rep.name = stats.name;
+        rep.round = round;
+        if (round === 6) rep.round = "Total";
+        round++;
+        Object.assign(rep, st);
+        arr.push(rep);
+      });
+    });
+    const options = {
+      delimiter: {
+        wrap: '"',
+        field: "\t",
+        eol: "\n"
+      }
+    };
+    const csv = await json2csvAsync(arr, options);
+    const myBlob = new Blob([csv], {
+      type: "octet/stream"
+    });
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(myBlob);
+    link.setAttribute("download", "IdleSpace2_" + report.name + ".csv");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 }
