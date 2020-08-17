f6287ca53ed9b1cc8dbe4285194efe549bb44f85
scorzy <scorzy88@gmail.com>
2020-08-17 17:10:49 +0200

    themes

--- src/app/options/ui/ui.component.ts
+++ src/app/options/ui/ui.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { MainService } from "src/app/main.service";
 import { OptionsService } from "src/app/options.service";
+import compiledCss from "../../model/data/themes.json";
+import startCase from "lodash-es/startCase";
 
 @Component({
   selector: "app-ui",
@@ -10,12 +12,28 @@ import { OptionsService } from "src/app/options.service";
 })
 export class UiComponent implements OnInit {
   usaFormat = OptionsService.usaFormat;
+  themes = [];
   constructor(public ms: MainService, public os: OptionsService) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.themes = compiledCss.map((theme) => {
+      let str = theme;
+      const end = theme.indexOf(".");
+      if (end > 0) {
+        str = str.substring(0, end);
+      }
+      return {
+        name: startCase(str.replace(/-/g, " ")),
+        css: str
+      };
+    });
+  }
   changeUsaFormat() {
     OptionsService.usaFormat = this.usaFormat;
   }
   onFormatChange() {
     this.os.generateFormatter();
+  }
+  getThemeId(index: number, theme: string) {
+    return index + theme;
   }
 }
--- src/makeThemes.js
+++ src/makeThemes.js
@@ -5,14 +5,57 @@ const fsExtra = require("fs-extra");
 const LessPluginCleanCSS = require("less-plugin-clean-css");
 const darkThemeVars = require("ng-zorro-antd/dark-theme");
 const compactThemeVars = require("ng-zorro-antd/compact-theme");
+const aliyunTheme = require("@ant-design/aliyun-theme").default;
 
 const darkCommon = require("./themes-vars/darkCommon.js");
 const myDarkVars = require("./themes-vars/myDarkVars.js");
 const commonModyVar = require("./themes-vars/commonModyVar");
-const green = {
-  "@primary-color": "@green-6"
+
+const smallText = {
+  "@font-size-sm": "12px",
+  "@font-size-base": "12px"
 };
-const toBuild = [
+const colors = [
+  { name: "blue", vars: null },
+  {
+    name: "green",
+    vars: {
+      "@primary-color": "@green-6"
+    }
+  },
+  {
+    name: "cyan",
+    vars: {
+      "@primary-color": "@cyan-6"
+    }
+  },
+  {
+    name: "geekblue",
+    vars: {
+      "@primary-color": "@geekblue-6"
+    }
+  },
+  {
+    name: "purple",
+    vars: {
+      "@primary-color": "@purple-6"
+    }
+  },
+  {
+    name: "volcano",
+    vars: {
+      "@primary-color": "@volcano-6"
+    }
+  },
+  {
+    name: "orange",
+    vars: {
+      "@primary-color": "@orange-6"
+    }
+  }
+];
+
+const base = [
   {
     name: "dark-blue-2",
     source: "dark-blue-2",
@@ -22,18 +65,6 @@ const toBuild = [
       ...darkCommon,
       ...myDarkVars,
       ...commonModyVar
-    }
-  },
-  {
-    name: "dark-green-2",
-    source: "dark-blue-2",
-    modifyVars: {
-      ...darkThemeVars,
-      ...compactThemeVars,
-      ...darkCommon,
-      ...myDarkVars,
-      ...commonModyVar,
-      ...green
     }
   },
   {
@@ -47,17 +78,6 @@ const toBuild = [
     }
   },
   {
-    name: "dark-green",
-    source: "dark-blue",
-    modifyVars: {
-      ...darkThemeVars,
-      ...compactThemeVars,
-      ...darkCommon,
-      ...commonModyVar,
-      ...green
-    }
-  },
-  {
     name: "light-blue",
     source: "light-blue",
     modifyVars: {
@@ -66,44 +86,71 @@ const toBuild = [
     }
   },
   {
-    name: "light-green",
+    name: "aliyun-blue",
     source: "light-blue",
     modifyVars: {
       ...compactThemeVars,
-      ...commonModyVar,
-      ...green
+      ...aliyunTheme,
+      ...commonModyVar
     }
   }
 ];
+
+const toBuild = [];
+base.forEach((theme) => {
+  // toBuild.push(theme);
+  colors.forEach((color) => {
+    const newTheme = {};
+    newTheme.name = theme.name.replace("blue", color.name);
+    newTheme.source = theme.source;
+    newTheme.modifyVars = { ...theme.modifyVars, ...color.vars };
+    toBuild.push(newTheme);
+    if (!theme.name.includes("aliyun")) {
+      const smallNew = {};
+      smallNew.name = newTheme.name + "-small";
+      smallNew.source = newTheme.source;
+      smallNew.modifyVars = { ...newTheme.modifyVars, ...smallText };
+      toBuild.push(smallNew);
+    }
+  });
+});
+
 const built = [];
 fsExtra.emptyDirSync("./src/assets/themes/");
 
+let i = 0;
 toBuild.forEach(function (theme) {
-  fs.readFile("./src/themes/" + theme.source + ".less", function (error, data) {
-    data = data.toString();
-    less.render(
-      data,
-      {
-        javascriptEnabled: true,
-        compress: true,
-        plugins: [new LessPluginCleanCSS({ advanced: true })],
-        modifyVars: theme.modifyVars
-      },
-      function (e, css) {
-        var hash = md5(css.css);
-        fs.writeFileSync(
-          "./src/assets/themes/" + theme.name + "." + hash + ".css",
-          css.css
-        );
-        built.push(theme.name + "." + hash + ".css");
-        if (toBuild.length === toBuild.length) {
-          fs.writeFile(
-            "./src/app/model/data/themes.json",
-            JSON.stringify(built, null, " "),
-            function () {}
+  setTimeout(() => {
+    fs.readFile("./src/themes/" + theme.source + ".less", function (
+      error,
+      data
+    ) {
+      data = data.toString();
+      less.render(
+        data,
+        {
+          javascriptEnabled: true,
+          compress: true,
+          plugins: [new LessPluginCleanCSS({ advanced: true })],
+          modifyVars: theme.modifyVars
+        },
+        function (e, css) {
+          var hash = md5(css.css);
+          fs.writeFileSync(
+            "./src/assets/themes/" + theme.name + "." + hash + ".css",
+            css.css
           );
+          built.push(theme.name + "." + hash + ".css");
+          if (toBuild.length === toBuild.length) {
+            fs.writeFile(
+              "./src/app/model/data/themes.json",
+              JSON.stringify(built, null, " "),
+              function () {}
+            );
+          }
         }
-      }
-    );
-  });
+      );
+    });
+  }, 3000 * i);
+  i++;
 });
