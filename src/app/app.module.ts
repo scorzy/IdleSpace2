53246c3647a584411061bed1e003fee896833da6
Scorzy <scorzy88@gmail.com>
2021-03-05 18:54:00 +0100

    ui fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -134,6 +134,7 @@ import { UnitTabsComponent } from "./unit-list/unit-tabs/unit-tabs.component";
 import { UnitOptionsComponent } from "./unit-list/unit-options/unit-options.component";
 import { BuyMenuComponent } from "./units/buy-menu/buy-menu.component";
 import { NzDividerModule } from "ng-zorro-antd/divider";
+import { UiScrollModule } from "ngx-ui-scroll";
 
 registerLocaleData(en);
 
@@ -277,7 +278,8 @@ const ngZorroConfig: NzConfig = {
     NzDropDownModule,
     NzPopconfirmModule,
     NzMessageModule,
-    NzDividerModule
+    NzDividerModule,
+    UiScrollModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US },
